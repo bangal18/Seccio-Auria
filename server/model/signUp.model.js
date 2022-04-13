ffff4abfba209919b5e567d30edc8f678c12e903
bangal18 <74682095+bangal18@users.noreply.github.com>
2022-04-14 00:55:05 +0200

    ja funcioanan los callbacks y tambien la implementacio basica delos tokens esta echa

--- server/model/signUp.model.js
+++ server/model/signUp.model.js
@@ -1,15 +1,16 @@
 const connection = require('../config/db').connection;
 
-exports.addUser = function (info) {
-    console.log(info);
-    // connection.connect(function(err){
-    //     if(err) console.log("Error connection");
-        
-    //     connection.query("SELECT * FROM news", function (err, result,fields){
-    //         if(err) console.log("Error");
-    //         console.log(result)
-    //     })
-    //     connection.end();
-      
-    // })
+exports.addUser = function (user) {
+    console.log(user)
+    connection.connect(function(err){
+        if(err){ console.log("Error connection"); return; }
+        let sql = "INSERT INTO users (role_id, name, nickname, photo ,email, password) VALUES (?,?,?,?,?,?)";
+        let values = [2,user.name,user.nickname,null,user.email,user.password]
+        connection.query(sql, values, function (err, result,fields){
+            if(err) throw err;
+            return { "code": 1, "lastId": result.insertId };
+        })
+    })
+    connection.end();
 }
+
